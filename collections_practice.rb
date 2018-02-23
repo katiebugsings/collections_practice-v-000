0a4f00a645fa098e2ce8d292ea6d6a2bfbd14216
Katie Cassell <kcassell944@gmail.com>
2018-02-23 00:19:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,5 +7,21 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  array.sort { |x,y| }
+  array.sort { |x,y| x.length <=> y.length }
+end
+
+def swap_elements(array)
+   array[1], array[2] = array[2], array[1]
+   return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  for i in 0...arr.length
+    arr[i][2] = "$"
+  end
+  arr
 end
